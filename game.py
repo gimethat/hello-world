4da4be583430a3ec7a70ae55502e62dde22de7ab
Robert Jenks <robertjenks@Roberts-Air-2.attlocal.net>
2019-08-29 20:57:12 -0700

    Added too high or low checks

--- game.py
+++ game.py
@@ -3,12 +3,15 @@ from random import randint
 print "Welcome to our guessing game"
 
 number = randint(1, 10)
+tries = 0
 
 while True:
 	guess = raw_input("Enter in a number (1-10):")
+	tries += 1
 	if int(guess) == number:
-		print "You got it!"
+		print "You got it in {} tries".format(tries)
 		break
-	else:
-		print "Try again!"
-
+	elif int(guess) > number:
+		print "Too high!"
+	elif int(guess) < number:
+		print "Too low!"
